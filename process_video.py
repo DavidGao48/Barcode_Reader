ae87bf451f7db7db76800f0fe9466994d96e754a
DavidGao48 <38148469+DavidGao48@users.noreply.github.com>
2019-06-21 13:47:51 +0900

    Update process_video.py

--- process_video.py
+++ process_video.py
@@ -39,6 +39,15 @@ def process_video(video_path, output_path):
         if (framecount % 10 == 0):
             print("We are at frame {} and time roughly {}.".format(framecount, framecount / 30))
 
+        barcodes = pyzbar.decode(frame)
+        for barcode in barcodes:
+            barcodeData = barcode.data.decode("utf-8")
+            barcodeType = barcode.type
+            barcodeLoc = barcode.rect
+            cv2.putText(frame, "{}({})".format(barcodeData, barcodeType), (barcodeLoc.left, barcodeLoc.top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.rectangle(frame, (barcodeLoc.left, barcodeLoc.top), (barcodeLoc.left + barcodeLoc.width, barcodeLoc.top + barcodeLoc.height), (0, 255, 0), thickness = 2)
+            cv2.imshow("detected!", cv2.resize(frame, (960, 640)))
+
         processed_frame = preprocess_image(frame)
 
         for (processed_part, plain_part, location_box) in processed_frame:
@@ -52,20 +61,20 @@ def process_video(video_path, output_path):
             barcodes = pyzbar.decode(plain_part) + pyzbar.decode(processed_part)
 
             if (barcodes == []):
-                '''
                 if ((x2 - x) * (y2 - y) < 14000):
+                    cv2.putText(frame, "Failed to decode", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                     continue
                 # If we cannot decode the barcode, we use extra processing on the processed part
                 extra_processed = extra_processing(processed_part, plain_part, vertical_padding_removal=int((y2 - y) / 6))
-                #if (framecount % 10 ==0):
-                #    cv2.imshow("extra processed" , extra_processed)
+                if (framecount % 10 ==0):
+                    cv2.imshow("extra processed" , extra_processed)
                 barcodes = pyzbar.decode(extra_processed)
                 if (barcodes == []):
                     cv2.putText(frame, "Failed to decode", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                     continue
-                elif (len(barcodes) > 1):
-                    cv2.putText(frame, "This barcode was decoded as multiple barcodes. Probability of failure is high",
-                                (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                #elif (len(barcodes) > 1):
+                #    cv2.putText(frame, "This barcode was decoded as multiple barcodes. Probability of failure is high",
+                #                (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                 else:
                     for barcode in barcodes:
                         #                (x, y, w, h) = barcode.rect
@@ -75,7 +84,7 @@ def process_video(video_path, output_path):
                         barcodeType = barcode.type
 
                         text = "{}({})".format(barcodeData, barcodeType)
-                        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                         cv2.imshow("detected!", cv2.resize(frame, (960, 640)))
                         cv2.waitKey(100)
 
@@ -83,11 +92,11 @@ def process_video(video_path, output_path):
                             csv.write("{}, {}\n".format(datetime.datetime.now(), barcodeData))
                             csv.flush()
                             found.add(barcodeData)
-                '''
-                cv2.putText(frame, "Failed to decode", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                continue
-            elif (len(barcodes) > 1):
-                cv2.putText(frame, "This barcode was decoded as multiple barcodes. Probability of failure is high", (x, y - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+                # cv2.putText(frame, "Failed to decode", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                # continue
+            # elif (len(barcodes) > 1):
+                # cv2.putText(frame, "This barcode was decoded as multiple barcodes. Probability of failure is high", (x, y - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             #loop oer the detected barcodes
             for barcode in barcodes:
 #                (x, y, w, h) = barcode.rect
@@ -106,7 +115,7 @@ def process_video(video_path, output_path):
                     csv.flush()
                     found.add(barcodeData)
 
-        if (framecount % 1 == 0):
+        if (framecount % 10 == 0):
 
             for i in range (0, len(processed_frame)):
                 cv2.imshow("processed part " + str(i), processed_frame[i][0])
